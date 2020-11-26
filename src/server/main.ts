115741a2ae08209028b74dae9d2c34e3d3f72179
acmurthy <acmurthy@gmail.com>
2020-11-25 19:38:33 -0800

    Fixed typo

--- src/server/main.ts
+++ src/server/main.ts
@@ -24,8 +24,8 @@ serverManager.manage();
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-      console.log('Listening on host:' + server_host); 
+server.listen(server_port, server_host, function()) {
+      console.log('Listening on host: ' + server_host); 
       console.log('Listening on port: %d', server_port);
 }
 
