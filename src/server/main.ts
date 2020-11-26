444cdf3bbf0ed78832669fd34592eae61e139399
acmurthy <acmurthy@gmail.com>
2020-11-25 19:33:07 -0800

    Fixed host/port

--- src/server/main.ts
+++ src/server/main.ts
@@ -22,4 +22,10 @@ const serverManager = new ServerManager(webSocketServer, userDataProvider, nextG
 
 serverManager.manage();
 
-server.listen(9999, '0.0.0.0');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+      console.log('Listening on host:' + server_host); 
+      console.log('Listening on port: %d', server_port);
+}
+
